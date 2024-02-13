8353d035612e974a09c6881cb50ebb3568dfafa0
MatteoPas93 <pascucci-matteooutlook.it>
2024-02-13 18:12:40 +0100

    Update Footer.jsx

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         </h6>
         <div>
           <button>
-            <a href="./index.html">Top Page</a>
+            <a href="./index.html">Start Page</a>
           </button>
         </div>
         </div>
