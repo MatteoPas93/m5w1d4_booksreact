e3a6ad1891c698e644dd4639c6881770ca3e490c
MatteoPas93 <pascucci-matteooutlook.it>
2024-02-28 14:59:10 +0100

    spinner home

--- src/Components/Loading/loadingSpinner.jsx
+++
@@ -1,9 +0,0 @@
-// const LoadSpinner = () => {
-//      return (
-//         <div className="spinner-border text-success" role="status">
-//            <span className="sr-only"></span>
-//         </div>
-//      )
-// }
-
-// export default LoadSpinner
--- src/Components/Main/AllTheBooks/AllTheBooks.jsx
+++ src/Components/Main/AllTheBooks/AllTheBooks.jsx
@@ -1,17 +1,21 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import CardBook from "../../Card/SingleBook";
 import { useDispatch, useSelector } from "react-redux";
 import { allBooks, getBooks } from "../../../Reducer/booksSlice";
 import "./AllTheBooks.css";
-const EpicBooks = ({onBookClick}) => {
+import { Spinner } from "react-bootstrap";
+const EpicBooks = ({ onBookClick }) => {
+  const [loading, setLoading] = useState(true);
   const books = useSelector(allBooks);
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getBooks());
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
   }, [dispatch]);
 
-  
   const mapArray = (array) => {
     return array.map((book) => (
       <CardBook
@@ -28,7 +32,17 @@ const EpicBooks = ({onBookClick}) => {
 
   return (
     <div className="d-flex col-lg-8 flex-wrap justify-content-evenly gap-3">
-      {books && books.length > 0 ? (
+      {loading ? (
+        <Spinner
+          animation="border"
+          role="status"
+          variant="danger"
+          style={{ width: "200px", height: "200px" }}
+          className="spinner"
+        >
+          <span className="sr-only"></span>
+        </Spinner>
+      ) : books && books.length > 0 ? (
         mapArray(books)
       ) : (
         <div className="notFound-background">
