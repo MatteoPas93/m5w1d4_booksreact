76425d4516c946536e151403733baed647b789f1
MatteoPas93 <pascucci-matteooutlook.it>
2024-02-28 15:17:56 +0100

    error page

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from "react";
+import classes from "./StylePages/ErrorPage.module.css";
 
 const ErrorPage = () => {
   return (
-    <div>
-        <h2> Oops... Page not found!!</h2>
+    <div className={classes["container-error"]}>
+      <button className={classes["button-home"]}>
+        <a href="/"> Go Home!</a>
+      </button>
+      <img
+        className="w-100 h-100"
+        src="https://img.freepik.com/premium-vector/page-found_23-2147508322.jpg?w=740"
+        alt=""
+      />
     </div>
-  )
-}
+  );
+};
 
-export default ErrorPage
+export default ErrorPage;
